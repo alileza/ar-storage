7c30533b150e9f5577500fcf590254f795332561
Ali Reza <alirezayahya@gmail.com>
2015-05-13 19:11:43 +0700

    add max age on key

--- _storage.js
+++ _storage.js
@@ -1,14 +1,16 @@
-window._storage = new(function CembriStorage() {
+window._storage = new (function CembriStorage() {
     this.status = 'idle';
     this.storageType = 'localStorage';
     this.result = [];
     this.debug = true;
     this.encryption = true;
+    this.maxAge = 30 * 60 * 1000;
 
 
     CembriStorage.prototype.set = function (key, value, callback) {
         var storage = {
-            data: value
+            data: value,
+            created_at: new Date()
         };
         storage = JSON.stringify(storage);
         if (this.encryption) {
@@ -18,17 +20,38 @@ window._storage = new(function CembriStorage() {
         window[this.storageType].setItem(key, storage);
         if (this.debug) console.info('Set ' + key + ' : ' + value);
 
-        if (typeof (callback) == 'function') callback(true);
+        if (typeof (callback) == 'function')return callback(true);
     }
 
-    CembriStorage.prototype.get = function (key, callback) {
+    CembriStorage.prototype.get = function (key) {
         if (this.encryption) key = btoa(key);
         var storage = window[this.storageType].getItem(key);
+        
+        if(storage == null)return null;
+        
         if (this.encryption) storage = atob(storage);
+        
         storage = JSON.parse(storage);
         if (this.debug) console.info('Retrieve ' + key);
+        var now = new Date();
+        var created_at = new Date(storage.created_at);
 
-        if (typeof (callback) == 'function') callback(storage.data);
+        var age = now - created_at;
+        
+        if(this.maxAge != 0 && age > this.maxAge){
+            window[this.storageType].removeItem(key);
+        }
+        return storage.data;
+        //if (typeof (callback) == 'function')return callback(storage.data);
+    }
+    
+    CembriStorage.prototype.remove = function (key) {
+        if (this.encryption) key = btoa(key);
+        var storage = window[this.storageType].removeItem(key);
+        
+        if (this.debug) console.info('Removing key : ' + atob(key));
+        
+        return storage;
     }
     
     CembriStorage.prototype.check = function (key) {
